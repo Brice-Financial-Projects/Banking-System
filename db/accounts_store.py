c5f1c94223248db470776e3817d13a9004d8b7a9
Brice Nelson <bnelsonemail@icloud.com>
2025-06-13 23:52:41 -0400

    feat: Enhanced account creation and customer management
    - Added complete customer info storage with address details
    - Implemented account number generation with 12-digit random numbers
    - Added duplicate account check before creation
    - Updated accounts_db structure with full customer profile
    - Set base customer ID to start from 1000
    - Improved account existence validation
    - Added documentation for accounts_db structure

--- db/accounts_store.py
+++ db/accounts_store.py
@@ -1,4 +1,18 @@
-"""db/accounts_store.py"""
+"""
+db/accounts_store.py
+
+accounts_db {
+    cust_id: {
+    first_name: fname,
+    last_name: lname,
+    acct_num: acct_num,
+    address: address,
+    city: city,
+    state: state,
+    zip: zip,
+    }
+}
+"""
 
 
 accounts_db = {}
--- model/account.py
+++ model/account.py
@@ -21,44 +21,73 @@ class BankAccount:
         """initialize BankAccount class"""
         self.first_name = first_name
         self.last_name = last_name
-                
-    def create_cust_id(self, first_name, last_name):
+
+
+    def create_cust_id(self, first_name, last_name, address, city, state, zip):
         """checks for the last customer ID created and creates the next ID in sequential order."""
+        # Step 0:  Check datebase for existing account
+        if self.check_for_account(first_name, last_name):
+            print(f"Customer {first_name} {last_name} already exists. Skipping account creation.")
+            return None
+
         #Step 1.  Check database for the last customer ID
-        max_id = 0
+        max_id = 1000
         for cust_id in accounts_db.keys():
             if cust_id > max_id:
-                cust_id = max_id
+                max_id = cust_id
         
-        # Step 2:  Assign next customer ID to the new customer.
-        new_cust_id = cust_id + 1
-        accounts_db[new_cust_id] = {
+        # Step 2:  Assign next customer ID to the new customer and store customer info into a dict.
+        new_cust_id_info = max_id + 1
+        acct_num = self.create_account(first_name,last_name)
+        accounts_db[new_cust_id_info] = {
             'first_name' : first_name,
             'last_name' : last_name,
+            'acct_num': acct_num,
+            'address': address,
+            'city': city,
+            'state': state,
+            'zip': zip
         }
-        return new_cust_id
+        return new_cust_id_info
+
 
     def check_for_account(self, first_name, last_name):
         """Checks database for an existing account"""
-        for last_name, first_name in accounts_db.values():
-            if last_name in accounts_db.values() and first_name in accounts_db.values():
+        for data in accounts_db.values():
+            if data['first_name'] == first_name and data['last_name'] == last_name:
                 print(f'{first_name} {last_name} already exists in the database.')
-                pass
-            else:
-                self.create_cust_id(first_name, last_name)
-    
-    
+                return True
+
+        print(f'{first_name} {last_name} does not exist in the database, needs a new account setup.\nRun create_cust_id')
+        return False
+
+
     def create_account(self, first_name, last_name):
         """Creates an account"""
-        temp_account = []
-        acct = random.randint(0,9)
-        
-        temp_account.append(acct)
-        print(temp_account)
+
+        acct = [random.randint(0,9) for x in range(12)]
+        acct_num = ''.join(str(x) for x in acct)
+        if acct_num in (data['acct_num'] for data in accounts_db.values()):
+            return self.create_account(first_name, last_name)
+        else:
+            return acct_num
+
             
 
 
 bank_account = BankAccount("Brice" , "Nelson")
 brice = "Brice"
 nelson = "Nelson"
-bank_account.create_account(brice, nelson)
+address = '3650 Hampton Glen Place'
+city = 'Jacksonville'
+state = 'FL'
+zip = '32257'
+
+bank_account.create_cust_id(brice, nelson, address, city, state, zip)
+bank_account.check_for_account(brice, nelson)
+from pprint import pprint
+
+cust_id = bank_account.create_cust_id(brice, nelson, address, city, state, zip)
+pprint(accounts_db)
+
+# print(accounts_db)
