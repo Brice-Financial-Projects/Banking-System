6ae0d5f2d408fdeddcf54716ccecb7c250b8e5c9
Brice Nelson <bnelsonemail@icloud.com>
2025-06-14 15:28:04 -0400

    refactor: Updated the docstring on the balance and deposit methods

--- model/transactions.py
+++ model/transactions.py
@@ -17,13 +17,61 @@ class AccountTransactions:
 
 
     def balance(self):
-        """checks balance in the db and returns the current balance"""
+        """Retrieves and returns the current account balance from the database.
+
+        This method checks if the customer ID exists in the transaction database
+        and returns the current balance if found. The balance is both printed to
+        the console and returned as a value.
+
+        Returns:
+            float: The current account balance.
+
+        Raises:
+            ValueError: If the customer ID is not found in the database.
+
+        Examples:
+            >>> account = AccountTransactions('1001')
+            >>> account.balance()
+            $71.72
+            71.72
+        """
         if self.cust_id in transactions_db:
             balance = transactions_db[self.cust_id]['balance']
             print(f'${balance}')
             return balance
         else:
             raise ValueError(f'Customer ID {self.cust_id} not found in the database')
+
+
+    def deposit(self, deposit):
+        """
+        Deposits funds into the customer's account.
+        
+        This method adds the specified amount to the customer's current balance
+        in the transaction database. If the customer ID does not exist in the 
+        database, a ValueError is raised.
+        
+        Args:
+            deposit (float): The amount to deposit into the account. Must be a 
+                             positive number.
+        
+        Returns:
+            float: The new account balance after the deposit.
+        
+        Raises:
+            ValueError: If the customer ID is not found in the database.
+            ValueError: If the deposit amount is not positive.
+        
+        Examples:
+            >>> account = AccountTransactions('1001')
+            >>> account.deposit(100.50)
+            Deposited $100.50
+            $600.50
+            600.5
+        """
+
+
+
 
 
 cust_id = '1001'
